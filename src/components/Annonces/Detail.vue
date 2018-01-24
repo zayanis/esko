788df991bbda7d62e4d5a21cb40190edaeea28f5
zayanis <zayanis@gmail.com>
2018-01-24 16:02:13 +0100

    Add files via upload

---
+++ src/components/Annonces/Detail.vue
@@ -0,0 +1,44 @@
+<template >
+	<div class="ui middle aligned center aligned grid">
+				<div class="column">
+					<div class="ui center aligned page grid">
+						<div class="column">
+							<div class="ui left aligned segment">
+								<div class="ui form">
+									<div class="field">
+										<label for="mail">E-mail:</label>
+										<div class="ui icon input">
+											<input type="text" placeholder="mail" name="E-mail" id="mail" v-bind:value=this.selectedDemande.user[0].mail disabled /> <i class="mail icon"></i>
+
+										</div>
+									</div>
+									<div class="field">
+										<label for="telephone">Tel:</label>
+										<div class="ui icon input">
+											<input type="text" placeholder="Mot de passe" name="telephone" id="telephone"  v-bind:value=this.selectedDemande.user[0].telephone disabled /> <i class="call icon"></i>
+
+										</div>
+									</div>
+									  <div class="field">
+										<label for="adresse">Adresse:</label>
+										<div class="ui icon input">
+											<input type="text"   name="adresse" id="adresse"  v-bind:value=this.selectedDemande.user[0].adresse disabled /> <i class="location arrow icon"></i>
+
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+</template>
+
+<script>
+
+
+export default {
+  props: ['selectedDemande'],
+	
+}
+</script>
